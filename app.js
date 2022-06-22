bbdf6b1093b0e487748dc80d37ea6a0a15675890
Alessio <16mmstudios@gmail.com>
2022-06-22 17:13:27 +0200

    Odin Library Project

--- app.js
+++ app.js
@@ -18,11 +18,20 @@ dMode.addEventListener('change',()=>{
 })
 
 
-function book(title,author,pages,read){
+/*function book(title,author,pages,read){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+}*/
+
+class book{
+  constructor (title,author,pages,read){
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  }
 }
 
 formB.addEventListener('submit',(e)=>{
